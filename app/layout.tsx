008d3bf972b97b822478c9b1344e571f12ae9560
pereira-renan <duke.js@outlook.com>
2023-08-28 21:39:42 -0300

    feat: add initial project setup using nextjs + nextui template, adds basic pages setup, adds prettier and tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,63 +1,78 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
+'use client'
+
+import '@/styles/globals.css'
+import { Metadata } from 'next'
+import { siteConfig } from '@/config/site'
+import { fontSans } from '@/config/fonts'
+import { Providers } from './providers'
+import { Navbar } from '@/components/navbar'
+import { Link } from '@nextui-org/link'
+import clsx from 'clsx'
+import { Divider } from '@nextui-org/react'
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
-	],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
-};
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
-							</Link>
-						</footer>
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <div className="flex items-center gap-1 text-current">
+                Criado por{' '}
+                <Link
+                  isExternal
+                  href="https://www.github.com/pereira-renan/"
+                  title="aka Justin Medeiros"
+                >
+                  <span className="text-primary">Renan Pereira</span>
+                </Link>
+              </div>
+              <Divider className="mx-3" orientation="vertical" />
+              <div className="flex items-center gap-1 text-current">
+                Mantido pelos{' '}
+                <Link
+                  isExternal
+                  href="https://www.instagram.com/crossfitboqueirao/"
+                  title="CrossFit Boqueirão"
+                >
+                  <span className="text-primary">alunos da CFBOQ</span>
+                </Link>
+              </div>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
 }
--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,65 @@
-export type SiteConfig = typeof siteConfig;
+export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: 'Next.js + NextUI',
+  description: 'Make beautiful websites regardless of your design experience.',
+  navItems: [
     {
-      label: "Docs",
-      href: "/docs",
+      label: 'Home',
+      href: '/',
     },
     {
-      label: "Pricing",
-      href: "/pricing",
+      label: 'Índice',
+      href: '/recommendations',
     },
     {
-      label: "Blog",
-      href: "/blog",
+      label: 'Categorias',
+      href: '/categories',
     },
     {
-      label: "About",
-      href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
-};
+      label: 'Recomendar',
+      href: '/recommendations/submit',
+    },
+    {
+      label: 'Sobre',
+      href: '/about',
+    },
+  ],
+  navMenuItems: [
+    {
+      label: 'Profile',
+      href: '/profile',
+    },
+    {
+      label: 'Dashboard',
+      href: '/dashboard',
+    },
+    {
+      label: 'Projects',
+      href: '/projects',
+    },
+    {
+      label: 'Team',
+      href: '/team',
+    },
+    {
+      label: 'Calendar',
+      href: '/calendar',
+    },
+    {
+      label: 'Settings',
+      href: '/settings',
+    },
+    {
+      label: 'Help & Feedback',
+      href: '/help-feedback',
+    },
+    {
+      label: 'Logout',
+      href: '/logout',
+    },
+  ],
+  links: {
+    github: 'https://github.com/pereira-renan/',
+  },
+}
